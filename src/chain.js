40db47e9f30f42784a5e8da58b7ef56fe1e40f52
Nick Stokoe <github.wu-lee@noodlefactory.co.uk>
2017-03-04 11:36:25 +0000

    reindent, remove debug print

--- src/chain.js
+++ src/chain.js
@@ -24,68 +24,73 @@ function generate(barcode, selector){
         bbox = canvas.getBoundingClientRect(),
         width = bbox.width,
         height = bbox.height;
-    console.log(width, height);
-
+    
     d3.select(canvas)
         .attr('width', width)
         .attr('height', height)
     
-    .call(d3.drag()
-        .container(canvas)
-        .subject(dragsubject)
+        .call(d3.drag()
+              .container(canvas)
+              .subject(dragsubject)
         .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended));
-function ticked() {
-  context.clearRect(0, 0, width, height);
-  context.save();
-  context.translate(width / 2, height / 2);
-
-  context.beginPath();
-  links.forEach(drawLink);
-  context.strokeStyle = "#aaa";
-  context.stroke();
-
-  context.beginPath();
-  nodes.forEach(drawNode);
-  context.fill();
-  context.strokeStyle = "#fff";
-  context.stroke();
-
-  context.restore();
+              .on("drag", dragged)
+              .on("end", dragended));
+    
+    function ticked() {
+        context.clearRect(0, 0, width, height);
+        context.save();
+        context.translate(width / 2, height / 2);
+        
+        context.beginPath();
+        links.forEach(drawLink);
+        context.strokeStyle = "#aaa";
+        context.stroke();
+        
+        //  links.forEach(drawLink);
+        context.strokeStyle = "#aaa";
+        context.stroke();
+        
+        context.beginPath();
+        nodes.forEach(drawNode);
+        context.fill();
+        context.strokeStyle = "#fff";
+        context.stroke();
+        
+        context.restore();
+    }
+    
+    function dragsubject() {
+        return simulation.find(d3.event.x - width / 2, d3.event.y - height / 2);
+    }
+    
+    function dragstarted() {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d3.event.subject.fx = d3.event.subject.x;
+        d3.event.subject.fy = d3.event.subject.y;
+    }
+    
+    function dragged() {
+        d3.event.subject.fx = d3.event.x;
+        d3.event.subject.fy = d3.event.y;
+    }
+    
+    function dragended() {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d3.event.subject.fx = null;
+        d3.event.subject.fy = null;
+    }
+    
+    function drawLink(d) {
+        context.moveTo(d.source.x, d.source.y);
+        context.lineTo(d.target.x, d.target.y);
+    }
+    
+    function drawNode(d) {
+        context.moveTo(d.x + 3, d.y);
+        context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
+    }
 }
 
-function dragsubject() {
-  return simulation.find(d3.event.x - width / 2, d3.event.y - height / 2);
-}
-
-function dragstarted() {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d3.event.subject.fx = d3.event.subject.x;
-  d3.event.subject.fy = d3.event.subject.y;
-}
-
-function dragged() {
-  d3.event.subject.fx = d3.event.x;
-  d3.event.subject.fy = d3.event.y;
-}
-
-function dragended() {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d3.event.subject.fx = null;
-  d3.event.subject.fy = null;
-}
-
-function drawLink(d) {
-  context.moveTo(d.source.x, d.source.y);
-  context.lineTo(d.target.x, d.target.y);
-}
-
-function drawNode(d) {
-  context.moveTo(d.x + 3, d.y);
-  context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
-}
-}
 generate("01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ .$%-/", "#root");
     d3.select('#text').text("hello");
 
